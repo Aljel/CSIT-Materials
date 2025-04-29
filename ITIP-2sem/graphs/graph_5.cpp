2cd64a591a09c93a4ac2ecc66352fa9b25264b47
Aljel <makakwar@gmail.com>
2025-04-29 18:04:21 +0400

    feat(ITIP-2sem): solve last graph problem

--- ITIP-2sem/graphs/graph_5.cpp
+++ ITIP-2sem/graphs/graph_5.cpp
@@ -2,6 +2,8 @@
 #include <vector> 
 #include <string>
 #include <fstream>
+#include <queue> 
+#include <algorithm>
 
 // функция для создания графа как списка смежности из файла 
 std::vector<std::vector<int>> graphBuildFromFile(std::string filename){
@@ -54,11 +56,61 @@ void graphPrint(std::vector<std::vector<int>> Gr){
         std::cout << "\n";
     }
 }
+
+std::vector<int> bfs (std::vector<std::vector<int>> Gr, int start, int end){
+    int n = Gr.size();
+    std::vector<bool> visited(n, false);
+    std::queue<int> q;
+    std::vector<int> parent(n, -1); // Массив для восстановления пути
+    visited[start] = 1;
+    q.push(start);
+
+    while(!q.empty()){
+        int y = q.front(); // Извлекаем голову очереди
+        q.pop();
+
+        if (y == end) { // Если достигли целевой вершины
+            // Восстанавливаем путь от end к x
+            std::vector<int> shortest_path;
+            for (int v = end; v != -1; v = parent[v]) {
+                shortest_path.push_back(v);
+            }
+            std::reverse(shortest_path.begin(), shortest_path.end());
+            return shortest_path;
+        }
+
+        for (int i = 0; i < Gr[y].size(); ++i) { // Цикл по соседям вершины y
+            int neighbor = Gr[y][i];
+            if (!visited[neighbor]) { // Если сосед не посещён
+                visited[neighbor] = true; // Помечаем как посещённый
+                parent[neighbor] = y; // Запоминаем родителя
+                q.push(neighbor); // Помещаем в очередь
+            }
+        }
+    }
+
+    return {};
+}
+
+
 int main(){
     std::vector<std::vector<int>> Gr = graphBuildFromFile("input.txt");
 
     graphPrint(Gr);
 
+    std::cout << "Enter two points\n";
+    int A, B;
+    std::cin >> A >> B;
+
+    std::vector<int> path = bfs(Gr, A, B);
+
+    if (path.empty())
+        std::cout << "There is no way!\n";
+    else{
+        std::cout << "Shortest way is ";
+        for (int a : path)
+            std::cout << a << " ";
+    }
     
     return 0;
 }
