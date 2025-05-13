aa91e673d5883cadec941b0d013bc78c2f4176b6
Aljel <makakwar@gmail.com>
2025-05-13 23:39:59 +0400

    feat(ITIP-2sem): solve first hash problem

--- ITIP-2sem/hash/hash_1.cpp
+++ ITIP-2sem/hash/hash_1.cpp
@@ -4,6 +4,25 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <string>
+
+people peopleMaker(){
+    people guy;
+    std::cout<< "Enter guy's surname\n";
+    std::cin >> guy.surname;
+    std::cout<< "Enter guy's work\n";
+    std::cin >> guy.work;
+    std::cout<< "Enter guy's date of birth\n";
+    std::string date;
+    std::cin >> date;
+    guy.dateOfBirth = strToDate(date);
+    std::cout<< "Enter guy's time of work\n";
+    std::cin >> guy.timeOfWork;
+    std::cout<< "Enter guy's salary\n";
+    std::cin >> guy.salary;
+
+    return guy;
+}
 
 int main() {
     std::cout << "Enter the size of the table\n";
@@ -11,10 +30,24 @@ int main() {
     std::cin >> M;
 
     std::vector<people> A = makePeopleVec();
-    for (int i = 0; i < A.size(); i++){
-        printPeople(A[i]);
-    }
+
+    std::vector<list*> T = createOpenHashTable(A, M);
+    printHashTable(T);
+
+    std::cout<< "Enter people type guy to test deletion\n";
+    people delguy = peopleMaker();
+    deleteOpenHashTable(T, delguy);
+
+    std::cout<< "Enter people type guy to test insertion\n";
+    people insertguy = peopleMaker();
+    insertOpenHashTable(T, insertguy);
+
+    std::cout<< "Enter guy salary to test search\n";
+    int salary;
+    std::cin >> salary;
+    printPeople(searchOpenHashTable(T, salary));
+
+    printHashTable(T);
 
     return 0;
 }
- 
